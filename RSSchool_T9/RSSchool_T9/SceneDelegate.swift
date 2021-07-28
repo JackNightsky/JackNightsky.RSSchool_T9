47a42a1be6e66a6cb7917eab94b869b96a62a44b
JackNigthsky <jack.shapyalevich@gmail.com>
2021-07-29 02:13:57 +0300

    feat: implement tabBarController

--- RSSchool_T9/RSSchool_T9/SceneDelegate.swift
+++ RSSchool_T9/RSSchool_T9/SceneDelegate.swift
@@ -14,9 +14,50 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = scene as? UIWindowScene else {
+        guard let scene = scene as? UIWindowScene else {
             fatalError("LOL, be careful, drink some water")
         }
+        
+        window = UIWindow(windowScene: scene)
+        
+        window?.rootViewController = self.rootViewController()
+        window?.makeKeyAndVisible()
     }
-}
 
+    // make tabBarViewController
+    func rootViewController() -> UIViewController {
+        // items ViewController  plug
+        let itemsViewController = UIViewController() // will be change on custom itemsVC
+        
+        itemsViewController.view.backgroundColor = .orange
+        /// set tabBarItem appearance
+        itemsViewController.tabBarItem = UITabBarItem.init(title: "Items", image: UIImage.init(systemName: "square.grid.2x2"), tag: 0)
+        
+        // settings ViewController plug
+        let settingsViewController = UIViewController() // will be change on custom itemsVC
+        
+        settingsViewController.view.backgroundColor = .purple
+        /// set tabBarItem appearance
+        settingsViewController.tabBarItem = UITabBarItem.init(title: "Settings", image: UIImage.init(systemName: "gear"), tag: 0)
+        
+        
+        //MARK: - TabBarController settings
+        let tabBarController = UITabBarController()
+        var tabBarControllers: [UIViewController] = []
+        /// add viewControllers
+        tabBarControllers.append(itemsViewController)
+        tabBarControllers.append(settingsViewController)
+        tabBarController.viewControllers = tabBarControllers
+        /// set VC to default
+        tabBarController.selectedViewController = itemsViewController
+        /// off btn edit
+        tabBarController.customizableViewControllers = nil
+        /// set appearance
+        tabBarController.tabBar.tintColor = .red
+        tabBarController.tabBar.barTintColor = .white
+        
+        
+        return tabBarController
+    }
+
+}
